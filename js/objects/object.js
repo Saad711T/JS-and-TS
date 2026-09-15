9be9f693eaf6af6d9677776af549846590c59452
0xSaad <75455552+Saad711T@users.noreply.github.com>
2026-09-15 16:54:54 +0300

    Create object.js

---
+++ js/objects/object.js
@@ -0,0 +1,12 @@
+let person = {
+    name : "Ahmad",
+    age : 18,
+    id : 155,
+    student : true
+}
+
+// Full person informations
+console.log(person);
+
+// Just his name
+console.log("Name :" + person.name);
