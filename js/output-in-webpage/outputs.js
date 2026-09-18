d9eae2fa0c3f348e0d3c2d90731e392a2626ffca
0xSaad <75455552+Saad711T@users.noreply.github.com>
2026-09-18 19:49:06 +0300

    Create outputs.js

---
+++ js/output-in-webpage/outputs.js
@@ -0,0 +1,8 @@
+alert("Hello World");
+// appears like an alert in browser with ok button
+
+prompt("Hello world");
+// appears like an message with prompt and ok button
+
+confirm("Hello world");
+// like an alert but with cancel button
